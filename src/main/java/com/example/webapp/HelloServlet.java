4a86c1a6975b4855e6764fbed7b9bb5321cb5cf7
Sytnyk Yurii <yoursit1980@gmail.com>
2023-04-25 23:12:11 +0300

    homework30

--- src/main/java/com/example/webapp/HelloServlet.java
+++ src/main/java/com/example/webapp/HelloServlet.java
@@ -5,12 +5,12 @@ import java.io.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet(name = "helloServlet", value = "/hello")
 public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "Hello, World!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
